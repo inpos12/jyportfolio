372bb434f43747da8dbf5a9e3a881b90b195e188
inpos12 <inpos_@naver.com>
2025-01-20 16:29:29 +0900

    2025.01.20

--- src/layout/LayoutComponents.jsx
+++ src/layout/LayoutComponents.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
   max-width: 100%;
@@ -8,12 +8,21 @@ export const Container = styled.div`
   padding: 0 120px;
   flex-wrap: wrap;
 `;
+const opacity = keyframes`
+ 0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+`;
 export const Row = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 33%;
   width: 100%;
   padding: 80px 0 0 0;
+  animation: ${opacity} 0.8s ease-in-out;
 `;
 export const Col = styled.div`
   max-width: 100%;
